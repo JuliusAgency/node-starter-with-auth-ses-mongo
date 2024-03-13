33f57e944db01f0b68809566a0041511cfb62826
evgeny <ekarpovsky@gmail.com>
2024-03-13 07:58:42 +0000

    Remove unused index.ts from authorization definitions

--- src/setup/authorization-definitions/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Examples of an authorization data definitions.
- * Use the definitions when a rules management
- * is not implemented.
- */
-
-export { populateRules, ModelType } from './populate';
